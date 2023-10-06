8fd3860f37a3777c5117a1ecb38a8a0782b6a376
Matszikas <sevecencomateus2001@gmail.com>
2023-10-06 10:42:44 -0300

    subindo algumas alterações

--- js/script.js
+++ js/script.js
@@ -52,3 +52,14 @@ function aumentaHeader() {
     header.classList.remove("min");
  }
 }
+
+function menuShow() {
+  let menuMobile = document.querySelector('.mobile-menu');
+  if (menuMobile.classList.contains('open')) {
+      menuMobile.classList.remove('open');
+      document.querySelector('.icon').src = "images/icons8-menu-squared-50 (1).png";
+  } else {
+      menuMobile.classList.add('open');
+      document.querySelector('.icon').src = "images/icons8-menu-squared-50.png";
+  }
+}
